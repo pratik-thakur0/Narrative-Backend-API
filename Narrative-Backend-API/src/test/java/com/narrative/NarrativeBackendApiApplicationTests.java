8575f4b0d8434fcdc9a42e1e9f16b2cf3c5de397
Pratik Thakur <pratikthakur034@gmail.com>
2025-03-06 04:28:14 +0530

    created project structure, created packages

--- Narrative-Backend-API/src/test/java/com/narrative/NarrativeBackendApiApplicationTests.java
+++ Narrative-Backend-API/src/test/java/com/narrative/NarrativeBackendApiApplicationTests.java
@@ -1,6 +1,5 @@
 package com.narrative;
 
-import com.narrative.Domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,23 +8,8 @@ class NarrativeBackendApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// .....
 	}
 
-	@Test
-	void testLombok(){
-		User user = new User();
-		user.setId(3);
-		user.setName("aman");
-		user.setBio("bro");
-
-		User user2 = User.builder()
-				.id(2)
-				.name("atul")
-				.bio("buddy")
-				.build();
-
-		System.out.println(user);
-		System.out.println(user2);
-	}
 
 }
